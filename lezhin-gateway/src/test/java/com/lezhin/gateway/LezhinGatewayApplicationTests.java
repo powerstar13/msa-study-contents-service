5690d502dec453012102aa4b359195162c988b4f
홍준성 <ghdwnstjd13@gmail.com>
2022-08-03 20:30:41 +0900

    Spring Initializr > API Gateway

---
+++ lezhin-gateway/src/test/java/com/lezhin/gateway/LezhinGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lezhin.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LezhinGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
