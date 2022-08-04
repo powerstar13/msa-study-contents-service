c18dcdaab3d2f2a3c3bd7ebd2c5d454a01b424a4
홍준성 <ghdwnstjd13@gmail.com>
2022-08-04 22:57:49 +0900

    Spring Initializr > History Domain

---
+++ lezhin-history/src/main/java/com/lezhin/history/LezhinHistoryApplication.java
@@ -0,0 +1,13 @@
+package com.lezhin.history;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LezhinHistoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LezhinHistoryApplication.class, args);
+    }
+
+}
